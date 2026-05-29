5ee805cf7a305d70b76a3f3fe745ac2e26e40714
felixbeeby-hub <felix.beeby@gmail.com>
2026-05-29 23:47:29 +0100

    Update snl-data.js

--- data/snl-data.js
+++ data/snl-data.js
@@ -116,7 +116,7 @@ window.SNL_DATA = {
           status: "current",
           role: "Repertory",
           seasons: [48, 49, 50, 51],
-          bio: "Despite usually doing the same couple bits/voices, they still land and are funny!<br>LEFT: Hernández during Weekend Update. CENTER LEFT: Hernández as the iconic and beloved 'Domingo'. CENTER RIGHT: Hernández (breaking) during the hilarious 'AERIAL TRAMWAY!!' sketch. RIGHT: Hernández during the SNL intro."
+          bio: "Despite usually doing the same couple bits/voices, they still land and are funny!<br>LEFT: Hernández during Weekend Update. CENTER LEFT: Hernández as the iconic and beloved 'Domingo'. CENTER RIGHT: Hernández (breaking) during the amazing 'AERIAL TRAMWAY!!' sketch. RIGHT: Hernández during the SNL intro."
         },
         sarah: {
           name: "Sarah Sherman",
@@ -130,11 +130,11 @@ window.SNL_DATA = {
         mikey: {
           name: "Mikey Day",
            photo: "mikey.jpg",
-           photobig: ["mikey.jpg", "mikey_spit.jpeg", "mikey_intro.jpg"],
+           photobig: ["mikey.jpg", "mikey_spit.jpeg", "mikey_tramway.jpeg", "mikey_intro.jpg"],
           status: "current",
           role: "Repertory",
           seasons: [42, 43, 44, 45, 46, 47, 48, 49, 50, 51],
-          bio: "The most memorable things are that he appears to be the oldest cast memeber and his name rhymes with Che's in the intro. His sketches are sometimes funny... and is often (maybe??) at the expense of him - like the Matt Damon sketch above.<br>LEFT: Day during a Cold Open. CENTER: Day during one of MANY spit takes in a sketch with Matt Damon. RIGHT: Day during the SNL intro"
+          bio: "The most memorable things are that he appears to be the oldest cast memeber and his name rhymes with Che's in the intro. His sketches are sometimes funny... and is often (maybe??) at the expense of him - like the Matt Damon sketch above.<br>LEFT: Day during a Cold Open. CENTER LEFT: Day during one of MANY spit takes in a sketch with Matt Damon. CENTER RIGHT: Day hilariously being the Aerial Tramway Emoji. RIGHT: Day during the SNL intro"
         },
         bowen: {
           name: "Bowen Yang",
