9726f3dfba3d644fc859977240bf31e1558b889c
felixbeeby-hub <felix.beeby@gmail.com>
2026-05-31 22:02:49 +0100

    Update snl-data.js

--- data/snl-data.js
+++ data/snl-data.js
@@ -86,10 +86,12 @@ window.SNL_DATA = {
         },
         kenan: {
           name: "Kenan Thompson",
+           photo: "kenan.jpg",
+           photobig: ["kenan.jpg", "kenan_host", "kenan_intro.jpg"],
           status: "current",
           role: "Repertory",
           seasons: [29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51],
-          bio: ""
+          bio: "He is, by far, the cast member with the largest tenure and has an iconic, 'innocent' smile. <br>LEFT: Thompson attending an event. CENTER: Thompson as a game show host during an SNL sketch (a common role). RIGHT: Thompson during the SNL intro."
         },
         veronika: {
           name: "Veronika Slowikowska",
